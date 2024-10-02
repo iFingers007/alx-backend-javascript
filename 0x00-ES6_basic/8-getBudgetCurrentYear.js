b2ca31e0b6f4f2a4068b11e0286428e446ef9af2
Abu Jehoiada <abujehoiada@gmail.com>
2024-10-02 11:36:02 +0100

    8-getBudgetCurrentYear.js

--- 0x00-ES6_basic/8-getBudgetCurrentYear.js
+++ 0x00-ES6_basic/8-getBudgetCurrentYear.js
@@ -10,7 +10,7 @@ export default function getBudgetForCurrentYear(income, gdp, capita) {
     [`income-${year}`]: income,
     [`gdp-${year}`]: gdp,
     [`capita-${year}`]: capita,
-  };  
+  };
 
   return budget;
 }
