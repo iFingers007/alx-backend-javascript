11833af1eb10d0295194a97665b96221dad74867
Abu Jehoiada <abujehoiada@gmail.com>
2024-10-08 14:50:01 +0100

    Added 9-groceries_list.js

---
+++ 0x03-ES6_data_manipulation/9-groceries_list.js
@@ -0,0 +1,10 @@
+export default function groceriesList() {
+    const grocList = new Map();
+    grocList.set('Apples', 10);
+    grocList.set('Tomatoes', 10);
+    grocList.set('Pasta', 1);
+    grocList.set('Rice', 1);
+    grocList.set('Banana', 5);
+
+    return grocList;
+}
